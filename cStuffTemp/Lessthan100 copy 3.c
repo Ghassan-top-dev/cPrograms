8cd0031c0a8d9a1d8d9d21511abfa238f205a472
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-08-24 06:43:00 +0000

    Committing ./cStuffTemp/Lessthan100 copy 3.c

---
+++ cStuffTemp/Lessthan100 copy 3.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int inputNum; 
+
+    do{
+
+        printf("Enter a positive integer: ");
+        scanf("%d", &inputNum);
+
+    } while(inputNum <= 0); 
+    
+
+
+
+    
+    if(inputNum > 0 && inputNum < 100)
+    {
+        while (inputNum < 100)
+        {
+
+        inputNum = inputNum * 2;
+
+        printf("%d ", inputNum); 
+
+        if (inputNum * 2 > 100)
+        {
+            break;
+ 
+        }
+        
+        }
+
+
+    }
+    
+    
+    
+    
+
+
+    return 0; 
+}
+// Adding a timestamp to track commits
