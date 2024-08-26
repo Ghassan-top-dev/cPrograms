ab48763852c3465bf9778099ef267192229eba4c
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-08-26 06:43:00 +0000

    Committing ./cStuffTemp/2dArray copy 3.c

---
+++ cStuffTemp/2dArray copy 3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+
+
+
+int main(){
+
+    int rows = 10;
+    int cols = 10; 
+    int temp = 0; 
+
+    int arr2D[rows][cols];
+    int i, j;
+
+
+    //initalizer 
+
+    for(i = 0; i < rows; i++){
+
+        for (j = 0; j < cols; j++)
+        {
+            arr2D[i][j] = temp;
+            temp++; 
+
+        }
+    
+    } 
+
+
+    for(i = 0; i < rows; i++){
+
+        for (j = 0; j < cols; j++)
+        {
+            if(arr2D[i][j] < 10){
+
+                printf("%d  ", arr2D[i][j]);
+
+            }
+            else{
+
+                printf("%d ", arr2D[i][j]);
+
+            }
+
+        }
+        printf("\n"); 
+    
+    } 
+
+
+
+
+
+
+    return 0;
+}
+// Adding a timestamp to track commits
