735f93557ffa18341686dca81bc56074f1c05e96
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-09-14 06:43:00 +0000

    Committing ./cStuffTemp/memLeaker copy.c

---
+++ cStuffTemp/memLeaker copy.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+int main() {
+    for (int i = 0; i < 100000000; i++) {
+        int *leak = (int *)malloc(sizeof(int) * 1000);
+        // Memory is allocated but never freed
+    }
+    return 0;
+}
+
+// Adding a timestamp to track commits
