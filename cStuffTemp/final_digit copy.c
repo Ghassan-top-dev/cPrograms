95a16778b3badf1006d58c2f982ce0ac9f7afffa
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-09-20 06:43:00 +0000

    Committing ./cStuffTemp/final_digit copy.c

---
+++ cStuffTemp/final_digit copy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int userInput; 
+    int lastDigit;
+
+    printf("Enter your number and I will return the final digit of the number: ");
+
+    scanf("%d", &userInput); 
+
+    lastDigit = userInput % 10; 
+
+    printf("your last digit is: %d", lastDigit); 
+
+
+    return 0;
+}
+// Adding a timestamp to track commits
