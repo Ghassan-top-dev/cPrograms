8905d48683239a34afa8975014cd52446e45ade0
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-10-10 06:43:00 +0000

    Committing ./cStuffTemp/averageHeight600 copy 2.c

---
+++ cStuffTemp/averageHeight600 copy 2.c
@@ -0,0 +1,30 @@
+/*THIS IS FOR IN CLASS LECTURE*/
+
+
+
+#include <stdio.h>
+
+
+
+
+int main(){
+
+float height;
+float sum = 0.0; 
+float average;
+
+while(height > 0){
+
+    sum = sum + height; 
+
+    printf("Enter height: ");
+    scanf("%f", &height);
+
+}
+
+average = sum / height;
+printf("%f", sum); 
+
+    return 0; 
+}
+// Adding a timestamp to track commits
