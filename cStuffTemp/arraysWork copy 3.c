97a4005eeb344b2c3948b3ce6b748d2346afd655
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-10-17 06:43:00 +0000

    Committing ./cStuffTemp/arraysWork copy 3.c

---
+++ cStuffTemp/arraysWork copy 3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+
+    int operationNum, i, ask;
+
+    printf("Enter how many elements you want: ");
+    scanf("%d", &operationNum);
+
+    // Initialize the array with the size entered by the user
+    int arr[operationNum];
+
+    printf("Enter the numbers in the array: ");
+    for (i = 0; i < operationNum; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("hi: %d n %d", arr[i]+ arr[i+1]); 
+
+
+
+    return 0; 
+}
+// Adding a timestamp to track commits
