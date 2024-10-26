fc74757ad8700461270f487fa7af9d8ada9b0131
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-10-26 06:43:00 +0000

    Committing ./cStuffTemp/flight_pts copy.c

---
+++ cStuffTemp/flight_pts copy.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int milesTravelled, bonus;  //variables for input and output
+
+    printf("Enter the travelled miles: ");
+    scanf("%d", &milesTravelled); //scans the amount of miles travelled 
+
+
+    //calculates the bonus
+    if(milesTravelled > 5000){
+        bonus = 100;
+    }
+    else if(milesTravelled > 3000 && milesTravelled < 5000){
+        bonus = 60;
+    }
+    else{
+        bonus = 10; 
+    }
+
+    printf("bonus = %d\n", bonus); //prints the bonus
+
+
+    return 0; 
+}
+// Adding a timestamp to track commits
