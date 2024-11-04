69c08e15283b2705f373171e40065018d2b17282
Ghassan Murad Agha <gmuradag@uoguelph.ca>
2024-11-03 22:05:40 -0500

    Create 123.c
    creates 123 pattern:
    
    1
    12
    123

---
+++ 123.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+    int i = 1;
+    int j; 
+
+    while (i<=3){
+
+        for (j=1; j <=i; j++)
+        {
+            printf("%d", j); 
+        }
+
+        printf("\n");
+        i=i+1; 
+
+    }
+
+
+
+    return 0; 
+}
