01d4dbec96591b12bd75d5c8b149cc62267a93f3
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-11-05 06:50:00 +0000

    Committing ./cStuffTemp/ptrSquare.c

--- cStuffTemp/ptrSquare.c
+++ cStuffTemp/ptrSquare.c
@@ -32,3 +32,4 @@ void squareInt (int aNumber, int * result){
 // Adding a timestamp to track commits
 // Adding a timestamp to track commits
 // Adding a timestamp to track commits
+// Adding a timestamp to track commits
