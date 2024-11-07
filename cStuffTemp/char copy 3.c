b412f2e9eb3ed7cdad214bc93fe48eed8eeec39a
Ghassan-top-dev <gmuradag@uoguelph.ca>
2024-11-07 06:43:00 +0000

    Committing ./cStuffTemp/char copy 3.c

---
+++ cStuffTemp/char copy 3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char input;
+
+    printf("Enter a character: ");
+    scanf("%c",&input);
+
+    if (isdigit(input) == 0)
+         printf("%c is not a digit.",input);
+    else
+         printf("%c is a digit.",input);
+    return 0;
+}
+// Adding a timestamp to track commits
