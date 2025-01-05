5106a6e9bc957929cf42076b7fb6540797492aa4
Ghassan-top-dev <gmuradag@uoguelph.ca>
2025-01-05 06:43:00 +0000

    Committing ./cStuffTemp/firstDig copy 2.c

---
+++ cStuffTemp/firstDig copy 2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int firsrDigit (int);
+
+int main(){
+
+
+
+
+
+    printf("Enter a number: ");
+    scanf("%d",*******)
+
+    return 0;
+}
+
+
+int firstDigit(int num){
+
+
+    int first;
+    while ( num!= 0)
+
+}
+// Adding a timestamp to track commits
