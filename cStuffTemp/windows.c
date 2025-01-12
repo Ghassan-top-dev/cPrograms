aefdf8d0de3f26e08696566f23e4be9818c40b23
Ghassan-top-dev <gmuradag@uoguelph.ca>
2025-01-12 06:43:00 +0000

    Committing ./cStuffTemp/windows.c

---
+++ cStuffTemp/windows.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(){
+
+
+    int dash; 
+    int space; 
+
+
+
+
+
+    for (dash = 0; dash < 2; dash++)
+    {
+        printf("+----------+----------+\n");
+        for (space = 0; space < 3; space++)
+        {
+            printf("|          |          |\n");
+        }
+
+
+        if (dash == 1)
+        {
+            printf("+----------+----------+\n");
+        }
+        
+    }
+    
+
+
+
+    return 0; 
+}
+
+
+
+
+
+// Adding a timestamp to track commits
